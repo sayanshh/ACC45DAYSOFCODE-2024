f430ff6912c814b445a809c317a47584f39d7804
sayanshh <sayanshnormal@gmail.com>
2024-10-22 20:23:41 +0530

    Add files via upload

---
+++ DAY - 20 CHEFLAND GAMES - EASY.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int t; 
+    scanf("%d", &t);
+
+    while (t--) {
+        int r1, r2, r3, r4; 
+        scanf("%d %d %d %d", &r1, &r2, &r3, &r4);
+
+        if (r1 == 0 && r2 == 0 && r3 == 0 && r4 == 0) {
+            printf("IN\n");
+        } else {
+            printf("OUT\n");
+        }
+    }
+
+    return 0;
+}
